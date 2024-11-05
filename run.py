1f2be5467da678f014d7c2f554e8a08d95ec72a0
josue-vy <josvillanuevay@upt.pe>
2024-11-05 16:43:14 -0500

    api-deeptruth-v1.1

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-from app import app
+from app import app 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
